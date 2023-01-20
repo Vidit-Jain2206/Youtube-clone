e77f94bdbb5e134c5337cc63739c39230e78084e
Vidit Jain <96811167+Vidit-Jain2206@users.noreply.github.com>
2023-01-20 23:29:30 +0530

    Header Section Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
-import './App.css';
+import React from 'react';
+import { AppContext } from './context/contextApi';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import SearchResult from "./components/SearchResult";
+import VideoDetails from "./components/VideoDetails";
 
 function App() {
   return (
-    <div className="text-5xl">
-     APP
-    </div>
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path='/' element={<Feed />} />
+            <Route path='/searchResults/:searchQuery' element={<SearchResult />} />
+            <Route path='/video:id' element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
   );
 }
 
---
+++ src/context/contextApi.js
@@ -0,0 +1,38 @@
+import React,{ useState, useEffect,createContext} from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context= createContext();
+
+export const AppContext=(props)=>{
+    const [loading,setLoading] = useState(false);
+    const [searchResults,setSearchResaults] = useState(false);
+    const [selectCategories,setSelectCategories] = useState("New");
+    const [mobileMenu,setMobileMenu] = useState(false);
+
+    useEffect(()=>{
+        fetchSelectedCategoryData(selectCategories);
+    },[selectCategories])
+
+    const fetchSelectedCategoryData=(query)=>{
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then((res)=>{
+            console.log(res)
+            setSearchResaults(res.contents)
+            setLoading(false)
+        })
+    }
+
+    return (
+        <Context.Provider value={{
+            loading,
+            setLoading,
+            searchResults,
+            selectCategories,
+            setSelectCategories,
+            mobileMenu,
+            setMobileMenu
+        }} >
+            {props.children}
+        </Context.Provider>
+    )
+}
